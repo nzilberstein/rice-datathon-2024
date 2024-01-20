4a8cec0486c6f408211a4d4131dd2fcc1d2062ee
JuanseHevia <jh216@rice.edu>
2024-01-20 15:20:46 -0600

    preprocessed v2 with corrected column dropping

---
+++ README.md
@@ -0,0 +1,12 @@
+# Notas de Facu 01-20
+
+- Primero tubo que va al fondo: torre de destilación
+    - Fondo del tubo: bh
+- Más stages en el lateral, mejor PR (stages de pipe)
+- Más stages en la columna, peor PR (stages de destilación)
+
+- bin lateral length: longitud total del tubo post bottom-hole
+
+- buena idea clusterizar por surface X y Y
+
+# Features we're using
--- utils.py
+++ utils.py
@@ -7,6 +7,17 @@ DATA_PATH = os.path.join(os.path.join(os.path.dirname(__file__), 'data'), "train
 DROP_COLS = [
     "number_of_stages",
     "frac_type",
+    "bh_x","bh_y",
+    "standardized_operator_name",
+    "average_stage_length",
+    "average_proppant_per_stage",
+    "average_frac_fluid_per_stage",
+    "average_stage_length",
+    "average_proppant_per_stage",
+    "average_frac_fluid_per_stage",
+    "proppant_to_frac_fluid_ratio",
+    "frac_fluid_to_proppant_ratio",
+    "frac_seasoning",
 ]
 
 RAW_DATA = pd.read_csv(DATA_PATH, index_col=0)
@@ -16,14 +27,16 @@ def preprocess_data(data):
     data.dropna(subset=["OilPeakRate"], inplace=True)
 
     data["surface_toe_len"] = np.sqrt((data.surface_x - data.horizontal_toe_x).to_numpy() ** 2 + (data.surface_y - data.horizontal_toe_y).to_numpy() ** 2)
-    data["surface_midpoint_len"] = np.sqrt((data.surface_x - data.horizontal_midpoint_x).to_numpy() ** 2 + (data.surface_y - data.horizontal_midpoint_y).to_numpy() ** 2)
-    data["surface_bh_len"] = np.sqrt((data.surface_x - data.bh_x).to_numpy() ** 2 + (data.surface_y - data.bh_y).to_numpy() ** 2)
     data["toe_midpoint_len"] = np.sqrt((data.horizontal_toe_x - data.horizontal_midpoint_x).to_numpy() ** 2 + (data.horizontal_toe_y - data.horizontal_midpoint_y).to_numpy() ** 2)
     data["toe_bh_len"] = np.sqrt((data.horizontal_toe_x - data.bh_x).to_numpy() ** 2 + (data.horizontal_toe_y - data.bh_y).to_numpy() ** 2)
     data["midpoint_bh_len"] = np.sqrt((data.horizontal_midpoint_x - data.bh_x).to_numpy() ** 2 + (data.horizontal_midpoint_y - data.bh_y).to_numpy() ** 2)
 
+    data["lateral_len"] = data.toe_midpoint_len * 2 # proxy a number of stages de pipe
+
 
     data["custom_average_proppant"] = data.total_proppant / data.surface_toe_len
+
+    data.drop(columns=DROP_COLS, inplace=True)
 
     data.to_csv(os.path.join(os.path.dirname(__file__), "data", "preprocessed.csv"), index=False)
 
